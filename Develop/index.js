7d5937b4f846ec35e6d49c5121cbd23b25a2ed37
Erica Breig <ericabreig@gmail.com>
2022-04-30 15:57:33 -0400

    added questions for generator

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,79 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        },
+        {
+            type: "input",
+            name: "user",
+            message: "Please enter your GitHub username: "
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of the project?",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Please describe the application"
+        },
+        {
+            type: "input",
+            name: "tableOfContents",
+            message: "Please type in your table of contents ",
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "How is this application installed? ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please describe the usage of the application"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "How do you want to license this project?",
+            choices: [
+                "Apache",
+                "Academic",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contribution",
+            message: "Please list any collaborators and any attributions"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Please describe any test functions for the application"
+        },
+    ]);
+  };
+  
+
+// const promptUser = () => {
+//     return inquirer.prompt([
+   
+//     ]);
+//   };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Good README Generator
+## Link to video of Application
+[Application video](https://drive.google.com/drive/folders/1mxcdHzG3yiLBd9thA1PziNMIIs7hLf0w?usp=sharing)
